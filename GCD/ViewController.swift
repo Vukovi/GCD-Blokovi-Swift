97c629d98e7147923a82a4ec32a747422046ea9c
Vukovi <Vuk@Users-MacBook-Pro.local>
2017-03-20 19:33:02 +0100

    komit blok

--- GCD/ViewController.swift
+++ GCD/ViewController.swift
@@ -3,22 +3,49 @@
 //  GCD
 //
 //  Created by Vuk on 2/18/17.
-//  Copyright © 2017 Vuk. All rights reserved.
+//  Copyright © 2017 Vuk. All rights reserved.//
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var IntedzerskiBlok: ((Int) -> Int)? = nil
+    
+    func deklarisiIPozoviBlokKvadrataSestice() {
+        var kvadrat = IntedzerskiBlok
+        kvadrat = { (broj) in
+            return broj * broj
+        }
+        print("Blok vraca vrednost \(kvadrat!(6))")
+    }
+    
+    func blokKaoFunkcija() {
+        var vreme = "kisovito"
+        print("Vreme je, pre nego sto sam ukljucio blok, \(vreme)")
+        
+        let promenaVremena: ((Void) -> Void) = {
+            vreme = "suncano"
+            print("Vreme unutar mog bloka je \(vreme)")
+        }
+        promenaVremena()
+    }
+    
+    func koriscenjeInlineBloka() {
+        let niz: NSArray = ["Selimovic","Andric","Ducic","Antic","Beckovic"]
+        niz.enumerateObjects({ (objekat, indeks, stop) in
+            let pisac: String = objekat as! String
+            print("Prezime pisca: \(pisac.uppercased())")
+        })
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        deklarisiIPozoviBlokKvadrataSestice()
+        blokKaoFunkcija()
+        koriscenjeInlineBloka()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
